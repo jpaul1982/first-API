92ea3a7eae7830899a601f814422a868c68c2f41
Paul Molnar <saintpaul55012@yahoo.com>
2019-11-30 16:37:46 -0600

    html logging in terminal

--- first_request.js
+++ first_request.js
@@ -1,7 +1,39 @@
 const request = require('request');
+const rp = require('request-promise')
 
-request('http://www.google.com', function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+// request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) =>  {
+//     if (error) {
+//         console.error('Houston, we have a problem:', error); // Print the error if one occurred
+
+//     } else {
+//         if (response.statusCode == 200) {
+//             const parsedData = JSON.parse(body);
+//             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//             console.log(`${parsedData.name} live in ${parsedData.address.city}`); // Print the HTML for the Google homepage.
+
+//         }
+//     }
+// });
+
+// rp('https://jsonplaceholder.typicode.com/users/1')
+//     .then((body) => {
+//         const parsedData = JSON.parse(body);
+//         console.log(`${parsedData.name} live in ${parsedData.address.city}`); // Print the HTML for the Google homepage.
+
+//     })
+//     .catch((err) => {
+//         console.log('error', err);
+        
+//     });
+
+
+rp('https://rapidapi.com/imdb/api/movie-database-imdb-alternative')
+.then((body) => {
+    // const parsedData = JSON.parse(body);
+    console.log(body); // Print the HTML for the Google homepage.
+
+})
+.catch((err) => {
+    console.log('error', err);
+    
 });
